542fb07b2181c4700192163c800a1afe749f8f20
Rodolphe Fontaine <30529563+Rodi26@users.noreply.github.com>
2022-06-01 15:18:11 +0200

    update variables

--- variables.tf
+++ variables.tf
@@ -1,3 +1,6 @@
 variable "ssh_key" {
   description= "Name of the variable created in IBM Cloud to manage the ssh key which allows to connect to the VSI
 }
+
+variable "region" { default = "us-south-1" }
+variable "environment" { default = "not-defined" }
